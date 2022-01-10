1181303e8adc4c94e2ddf2725b87b2278a69893b
markmolfird <61727859+markmolfird@users.noreply.github.com>
2022-01-10 05:51:20 +0000

    Add files via upload

--- data/interface/resources/listeners.js
+++ data/interface/resources/listeners.js
@@ -1,28 +1,62 @@
+var undo = document.getElementById("undo");
+var redo = document.getElementById("redo");
+var hint = document.getElementById("hint");
+var best = document.getElementById("best");
+var auto = document.getElementById("auto");
+var init = document.getElementById("init");
+var speed = document.getElementById("speed");
+var score = document.getElementById("score");
+var toggle = document.getElementById("toggle");
+var keyboard = document.querySelector(".keyboard");
+var settings = document.querySelector(".settings");
+var hsla = [...document.querySelectorAll("td[class='HSLA']")];
+
+var touch = {
+  "threshold": 30,
+  "container": document.getElementById("main"),
+  "swipe": {
+    "direction": null
+  },
+  "dist": {
+    'X': null, 
+    'Y': null
+  },
+  "start": {
+    'X': null, 
+    'Y': null
+  },
+  "time": {
+    "start": null, 
+    "elapsed": null, 
+    "allowed": 1000
+  }
+};
+
 document.addEventListener("keydown", function (e) {
   config.app.gesture(e.which);
 });
 
-document.getElementById("speed").addEventListener("change", function (e) {
+speed.addEventListener("change", function (e) {
   if (parseInt(e.target.value) + '' === e.target.value + '') {
     config.storage.write("speed", e.target.value);
   }
 });
 
-document.querySelector(".keyboard").addEventListener("click", function (e) {
+keyboard.addEventListener("click", function (e) {
   var data = e.target.getAttribute("data");
   var key = parseInt(data) || 0;
   config.app.gesture(key);
 });
 
-document.getElementById("toggle").addEventListener("click", function () {
-  var toggle = config.storage.read("toggle") !== undefined ? config.storage.read("toggle") : "hide";
-  toggle = toggle === "show" ? "hide" : "show";
-  config.storage.write("toggle", toggle);
+toggle.addEventListener("click", function () {
+  var state = config.storage.read("toggle") !== undefined ? config.storage.read("toggle") : "hide";
+  state = state === "show" ? "hide" : "show";
+  config.storage.write("toggle", state);
   /*  */
-  document.querySelector(".settings").setAttribute("toggle", toggle);
+  settings.setAttribute("toggle", state);
 });
 
-document.getElementById("undo").addEventListener("click", function () {
+undo.addEventListener("click", function () {
   var state = config.storage.read("O_state");
   if (state !== undefined) {
     config.app.matrix.cell.e.grid = state.grid;
@@ -31,7 +65,7 @@ document.getElementById("undo").addEventListener("click", function () {
   }
 });
 
-document.getElementById("redo").addEventListener("click", function () {
+redo.addEventListener("click", function () {
   var state = config.storage.read("N_state");
   if (state !== undefined) {
     config.app.matrix.cell.e.grid = state.grid;
@@ -40,7 +74,7 @@ document.getElementById("redo").addEventListener("click", function () {
   }
 });
 
-document.getElementById("hint").addEventListener("click", function (e) {
+hint.addEventListener("click", function (e) {
   var i = config.app.predict(config.app.kernel.AI, false);
   e.target.textContent = ['↑', '→', '↓', '←'][i];
   /*  */
@@ -49,42 +83,42 @@ document.getElementById("hint").addEventListener("click", function (e) {
   }, 1000);
 });
 
-document.getElementById("best").addEventListener("click", function () {
+best.addEventListener("click", function () {
   var reset = window.confirm("Do you want to reset best score?");
   if (reset) {
     config.storage.write("besttile", 0);
     config.storage.write("bestscore", 0);
     /*  */
     var bestscore = config.storage.read("bestscore") + ' (' + config.storage.read("besttile") + ')';
-    document.getElementById("best").textContent = config.storage.read("bestscore") !== undefined ? bestscore : 0;
+    best.textContent = config.storage.read("bestscore") !== undefined ? bestscore : 0;
   }
 });
 
-document.getElementById("auto").addEventListener("click", function () {
+auto.addEventListener("click", function () {
   if (!config.app.kernel.AI.running) {
     config.app.kernel.AI.running = true;
-    this.textContent = 'STOP';
+    this.textContent = "STOP";
     config.app.play.auto();
   } else {
     config.app.kernel.AI.running = false;
-    this.textContent = 'Auto';
+    this.textContent = "Auto";
   }
 });
 
-document.getElementById("init").addEventListener("click", function () {
+init.addEventListener("click", function () {
   config.storage.write("storage", '');
   config.app.init(config.app.kernel.AI);
   config.app.update(config.app.kernel.AI);
   /*  */
-  document.getElementById("hint").textContent = "?";
-  document.getElementById("score").textContent = "0";
-  document.getElementById("auto").textContent = "Auto";
+  hint.textContent = "?";
+  score.textContent = "0";
+  auto.textContent = "Auto";
   /*  */
   var bestscore = config.storage.read("bestscore") + ' (' + config.storage.read("besttile") + ')';
-  document.getElementById("best").textContent = config.storage.read("bestscore") !== undefined ? bestscore : 0;
+  best.textContent = config.storage.read("bestscore") !== undefined ? bestscore : 0;
 });
 
-[...document.querySelectorAll("td[class='HSLA']")].map(function (elm) {
+hsla.forEach(function (elm) {
   elm.style.backgroundColor = "hsla(" + elm.getAttribute("hue") + ', ' + elm.getAttribute("saturation") + "%, " + elm.getAttribute("lightness") + "%, " + elm.getAttribute("alphavalue") + ')';
   /*  */
   elm.addEventListener("click", function (e) {
@@ -93,7 +127,53 @@ document.getElementById("init").addEventListener("click", function () {
     var saturation = parseInt(e.target.getAttribute("saturation"));
     var alphavalue = parseInt(e.target.getAttribute("alphavalue"));
     /*  */
-    config.app.color.HSLA = {"hue": hue, "lightness": lightness, "saturation": saturation, "alphavalue": alphavalue};
+    config.app.color.HSLA = {
+      "hue": hue, 
+      "lightness": lightness, 
+      "saturation": saturation, 
+      "alphavalue": alphavalue
+    };
+    /*  */
     config.app.update();
   });
 });
+
+/* touch */
+
+touch.container.addEventListener("touchmove", function (e) {
+  e.preventDefault();
+}, false);
+
+touch.container.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+  /*  */
+  var current = e.changedTouches[0];
+  touch.start.X = current.pageX;
+  touch.start.Y = current.pageY;
+  touch.swipe.direction = "none";
+  /*  */
+  touch.time.start = new Date().getTime();
+}, false);
+
+touch.container.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  /*  */
+  var current = e.changedTouches[0];
+  touch.dist.X = current.pageX - touch.start.X;
+  touch.dist.Y = current.pageY - touch.start.Y;
+  touch.time.elapsed = new Date().getTime() - touch.time.start;
+  /*  */
+  if (touch.time.elapsed <= touch.time.allowed) {
+    if (config.LOG) console.error("> X", Math.abs(touch.dist.X), "> Y", Math.abs(touch.dist.Y));
+    var vertical = Math.abs(touch.dist.X) >= touch.threshold && Math.abs(touch.dist.X) > Math.abs(touch.dist.Y);
+    var horizontal = Math.abs(touch.dist.Y) >= touch.threshold && Math.abs(touch.dist.Y) > Math.abs(touch.dist.X);
+    /*  */
+    if (vertical) {
+      touch.swipe.direction = (touch.dist.X < 0) ? 37 : 39;
+    } else if (horizontal) {
+      touch.swipe.direction = (touch.dist.Y < 0) ? 38 : 40;
+    }
+  }
+  /*  */
+  config.app.gesture(touch.swipe.direction);
+}, false);
